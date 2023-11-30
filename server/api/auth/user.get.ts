809ab1986e0d5d4d5b37cbaca37265542324f03f
Thomas Chin <tchin25@gmail.com>
2023-11-30 11:28:17 -0500

    simplify

--- server/api/auth/user.get.ts
+++ server/api/auth/user.get.ts
@@ -1,18 +1,6 @@
-import { H3Event } from "h3";
 // @ts-ignore
 import { verify } from "jsonwebtoken";
-const TOKEN_TYPE = "Bearer";
-const SECRET = "dummy";
-
-const extractToken = (authHeaderValue: string) => {
-  const [, token] = authHeaderValue.split(`${TOKEN_TYPE} `);
-  return token;
-};
 
 export default eventHandler((event) => {
-  const authHeaderValue = getRequestHeader(event, "authorization");
-  if (authHeaderValue) {
-    const extractedToken = extractToken(authHeaderValue);
-    return verify(extractedToken, SECRET);
-  }
+  return verify("test", "secret");
 });
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,18 +1,6 @@
 // @ts-ignore
-import jwt from "jsonwebtoken";
+import { verify } from "jsonwebtoken";
 
-const TOKEN_TYPE = "Bearer";
-const SECRET = "dummy";
-
-const extractToken = (authHeaderValue: string) => {
-  const [, token] = authHeaderValue.split(`${TOKEN_TYPE} `);
-  return token;
-};
-
-export default defineEventHandler(async (event) => {
-  const authHeaderValue = getRequestHeader(event, "authorization");
-  if (authHeaderValue) {
-    const extractedToken = extractToken(authHeaderValue);
-    jwt.verify(extractedToken, SECRET);
-  }
+export default defineEventHandler((event) => {
+  verify("test", "secret");
 });
