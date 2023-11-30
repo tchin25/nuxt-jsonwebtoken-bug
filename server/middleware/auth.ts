a17ee7bfba8dd07cfd40c871592bfb102b6894fe
Thomas Chin <tchin25@gmail.com>
2023-11-30 11:22:32 -0500

    check if server

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,21 +1,25 @@
-// // @ts-ignore
-// import { verify } from "jsonwebtoken";
+// @ts-ignore
+import jwt from "jsonwebtoken";
 
-// const TOKEN_TYPE = "Bearer";
-// const SECRET = "dummy";
+const TOKEN_TYPE = "Bearer";
+const SECRET = "dummy";
 
-// const extractToken = (authHeaderValue: string) => {
-//   const [, token] = authHeaderValue.split(`${TOKEN_TYPE} `);
-//   return token;
-// };
+const extractToken = (authHeaderValue: string) => {
+  const [, token] = authHeaderValue.split(`${TOKEN_TYPE} `);
+  return token;
+};
 
 export default defineEventHandler(async (event) => {
-  //   let name: string = "";
-  //   const authHeaderValue = getRequestHeader(event, "authorization");
-  //   if (authHeaderValue) {
-  //     const extractedToken = extractToken(authHeaderValue);
-  //     const jwtPayload = verify(extractedToken, SECRET, { complete: false });
-  //     name = jwtPayload["name"] || "";
-  //     event.context.auth = { name };
-  //   }
+  if (process.server) {
+    let name: string = "";
+    const authHeaderValue = getRequestHeader(event, "authorization");
+    if (authHeaderValue) {
+      const extractedToken = extractToken(authHeaderValue);
+      const jwtPayload = jwt.verify(extractedToken, SECRET, {
+        complete: false,
+      });
+      name = jwtPayload["name"] || "";
+      event.context.auth = { name };
+    }
+  }
 });
